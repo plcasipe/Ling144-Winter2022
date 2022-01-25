79240f2eee3b7e9d8dfad188e90090efca7f9255
Kelsey Kraus <knkraus@ucsc.edu>
2022-01-25 00:23:47 +0000

    Added Week 4 folder

---
+++ Week-4/loops-conditionals.py
@@ -0,0 +1,55 @@
+# A couple of practice problems with loops & conditionals.
+
+# The opening lines from William Butler Yeates'
+# The Second Coming, taken from
+# https://www.poetryfoundation.org/poems/43290/the-second-coming
+
+import re
+
+gyre = """Turning and turning in the widening gyre
+The falcon cannot hear the falconer;
+Things fall apart; the centre cannot hold;
+Mere anarchy is loosed upon the world,
+The blood-dimmed tide is loosed, and everywhere
+The ceremony of innocence is drowned;
+The best lack all conviction, while the worst
+Are full of passionate intensity."""
+
+#==============================
+# Main Goal:
+#   Make a Pig Latin Generator
+#   Generate the PigLatin version of 'gyre'
+#==============================
+
+# First: Let's ignore the poem. How do we make these into pig latin:
+#   - gyre
+#   - and
+
+
+
+# Second: how would we modify this for a word like
+#       'things'
+
+
+
+
+#------------------------------
+# Sub goals:
+#   - make all the data lowercase
+#   - get rid of punctuation
+#   - save the text into a list that we can manipulate
+#   - save information about what words are on what lines
+#   - write our pig latin generator
+#   - print our pig latin-ified poem
+#------------------------------
+
+# get rid of punctiation & make lowercase
+
+
+
+# save the text to a list
+
+
+
+# write our pig latin generator
+
